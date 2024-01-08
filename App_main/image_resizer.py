49af4ce824355f6c131bc91b6a01242c4937485f
PiotrekGelert <piotrekgelert@gmail.com>
2024-01-08 13:58:53 +0000

    reorganize app view

--- App_main/image_resizer.py
+++ App_main/image_resizer.py
@@ -3,6 +3,23 @@ import pathlib
 import re
 import sys
 
+import PIL
 import PyQt6.QtCore as qtc
 import PyQt6.QtGui as qtg
 import PyQt6.QtWidgets as qtw
+from PyQt6.QtCore import Qt
+from UI.image_resize_ui import Ui_mw_resize_image
+
+
+class ImageResizer(qtw.QMainWindow, Ui_mw_resize_image):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+
+if __name__ == '__main__':
+    app = qtw.QApplication(sys.argv)
+    window = ImageResizer()
+    window.show()
+    sys.exit(app.exec())
